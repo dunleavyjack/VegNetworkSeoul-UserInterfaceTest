79fffcf600ce0c6f9b2d535124d919b633b49610
dunleavyjack <dunleavyjack@gmail.com>
2021-05-03 11:06:46 +0900

    Tweaked

--- components/FindCard.js
+++ components/FindCard.js
@@ -27,7 +27,7 @@ const FindCard = ({ name, veg, tag, price, location }) => {
             really good!
           </p>
           <div className="find-location">
-            {location} <RoomIcon />
+            {location} <RoomIcon fontSize="small" />
             {/* <span className={"find-price"}>{"  " + price}</span> */}
           </div>
         </div>
